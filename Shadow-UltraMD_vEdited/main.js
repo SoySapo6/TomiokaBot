c39911d2df88cfb166fc938cd4b31e6a51d7807f
dosifoy627 <41215712-dosifoy627@users.noreply.replit.com>
2025-04-06 19:43:18 +0000

    Restored to 'fd7da0e1927f557797122615ea6767b53ac03704'
    Replit-Restored-To: fd7da0e1927f557797122615ea6767b53ac03704

--- Shadow-UltraMD_vEdited/main.js
+++ Shadow-UltraMD_vEdited/main.js
@@ -135,10 +135,8 @@ resolver(respuesta.trim())
 })})
 }
 
-if (!methodCodeQR && !methodCode && !fs.existsSync(`./${authFile}/creds.json`)) {
-opcion = '2' 
-phoneNumber = '+51921826291'
-}
+let opcion = '2'
+let phoneNumber = '+51921826291'
 
 const filterStrings = [
 "Q2xvc2luZyBzdGFsZSBvcGVu", // "Closing stable open"
