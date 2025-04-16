d40a71b657ea29994fa97170504319a58d59409f
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-04-16 02:02:23 -0500

    SoyMaycol

--- services/Youtubedescarga.js
+++ services/Youtubedescarga.js
@@ -4,6 +4,9 @@ const path = require("path");
 
 const descargarVideo = async (query) => {
     return new Promise((resolve, reject) => {
+        // Crear la carpeta si no existe
+        fs.mkdirSync('./videos', { recursive: true });
+
         const outputPath = `./videos/${Date.now()}.mp4`;
         const command = `yt-dlp -f best -o "${outputPath}" "ytsearch:${query}"`;
 
