d4eaef0a91469a2ae61bbcd2b14a47d5bd967ea6
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-04-16 22:29:59 -0500

    SoyMaycol

--- InstalacionAutomatica.sh
+++ InstalacionAutomatica.sh
@@ -1,6 +1,6 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash  
 
-clear
+clear  
 
 # Colores
 Cyan="\e[1;36m"
@@ -10,25 +10,12 @@ Red="\e[1;31m"
 Yellow="\e[1;33m"
 Reset="\e[0m"
 
-# Función para imprimir texto ASCII con estilo
-function print_ascii() {
-    echo -e "$Cyan"
-    figlet -f slant "$1"
-    echo -e "$Reset"
-}
-
-# Música de fondo con ffplay
-AUDIO_URL="https://files.catbox.moe/596y4p.m4a"
-AUDIO_FILE="$HOME/.maycol_music.m4a"
-
-if [ ! -f "$AUDIO_FILE" ]; then
-    echo -e "$PurpleDescargando música épica...$Reset"
-    wget -O "$AUDIO_FILE" "$AUDIO_URL"
-fi
-
-# Reproducir música en loop silencioso (background con PID controlado)
-ffplay -nodisp -autoexit -loglevel quiet "$AUDIO_FILE" &
-MUSIC_PID=$!
+# Función para imprimir texto ASCII con estilo  
+function print_ascii() {  
+    echo -e "$Cyan"  
+    figlet -f slant "$1"  
+    echo -e "$Reset"  
+}  
 
 # Presentación mágica
 CHARACTER_1="Hanako-kun $Yellow✨"
@@ -43,46 +30,47 @@ sleep 2
 print_ascii "MaycolAI"
 echo -e "$PurpleAjusta la Escala de la Pantalla para una mejor experiencia...$Reset"
 echo -e "$GreenHecho con amor por SoyMaycol$Reset"
-sleep 2
+sleep 2  
 
-# Actualización
-print_ascii "Actualizando"
+# Actualización  
+print_ascii "Actualizando"  
 echo -e "$Yellow$CHARACTER_1: ¡Vamos a actualizar todo antes de empezar!$Reset"
-apt update -y && yes | apt upgrade && pkg install -y bash wget figlet ffmpeg git nodejs-lts python-pip
+pkg update -y && pkg upgrade -y  
 
-# Instalación de herramientas esenciales
-print_ascii "Instalando"
+# Instalación de herramientas esenciales  
+print_ascii "Instalando"  
 echo -e "$Purple$CHARACTER_2: Instalando herramientas esenciales...$Reset"
-npm install -g yarn
-pip install yt-dlp
+pkg install git -y  
+pkg install nodejs-lts -y  
+pkg install ffmpeg -y  
+pkg install python-pip -y  
+npm install -g yarn  
+pip install yt-dlp  
 
-# Clonando repositorio
-print_ascii "Clonando Repo"
+# Clonando repositorio  
+print_ascii "Clonando Repo"  
 echo -e "$Green$CHARACTER_3: Descargando MaycolAI desde los cielos digitales...$Reset"
-git clone https://github.com/SoySapo6/MaycolAI
+git clone https://github.com/SoySapo6/MaycolAI  
 
-cd MaycolAI
+cd MaycolAI  
 
-# Instalando módulos del proyecto
-print_ascii "Dependencias"
+# Instalando módulos del proyecto  
+print_ascii "Dependencias"  
 echo -e "$Yellow$CHARACTER_1: Invocando todos los módulos necesarios...$Reset"
-npm install
-npm install gemini-chatbot
+npm install  
+npm install gemini-chatbot  
 
-# Eliminando sesiones antiguas
-print_ascii "Limpieza"
+# Eliminando sesiones antiguas  
+print_ascii "Limpieza"  
 echo -e "$Red$CHARACTER_3: Eliminando sesiones pasadas para evitar errores...$Reset"
-rm -rf baileys
+rm -rf baileys  
 
-# Mensaje final
-clear
-print_ascii "¡Listo!"
+# Mensaje final  
+clear  
+print_ascii "¡Listo!"  
 echo -e "$PurpleGracias por usar MaycolAI, eres lo máximo <3$Reset"
 echo -e "$GreenIniciando MaycolAI con poderes mágicos...$Reset"
-sleep 2
+sleep 2  
 
-# Detener música antes de iniciar el bot
-kill $MUSIC_PID
-
-# Iniciar el bot
+# Iniciando el bot  
 npm start
