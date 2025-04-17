cb1b4ecaf40adc718c940c01ca789870cdd25dd1
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-04-17 09:24:53 -0500

    SoyMaycol

--- InstalacionAutomatica.sh
+++ InstalacionAutomatica.sh
@@ -86,4 +86,4 @@ echo -e "$GreenIniciando MaycolAI con poderes mágicos...$Reset"
 sleep 2  
 
 # Iniciando el bot  
-npm start || (echo -e "$Red[Error]: El bot falló al iniciar.$Reset"; read -p "Presiona enter para salir...")
+npm start && exec bash || (echo -e "$Red[Error]: El bot falló al iniciar.$Reset"; read -p "Presiona enter para salir...")
