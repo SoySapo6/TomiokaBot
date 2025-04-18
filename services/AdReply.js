35d0692ec782e4bd21d1f13d983e985d78c2030d
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-04-18 11:36:43 -0500

    SoyMaycol

--- services/AdReply.js
+++ services/AdReply.js
@@ -4,12 +4,12 @@ function getAdReplyScript(title, body, thumbnailUrl, mediaUrl) {
         contextInfo: {
             externalAdReply: {
                 showAdAttribution: true,  // Mostrar atribución del anuncio
-                title: title || "Maycol AI",  // Título del mensaje de AdReply (se puede personalizar)
+                title: title || "SoyMaycol",  // Título del mensaje de AdReply (se puede personalizar)
                 body: body || "No olvides seguirme <3",  // Cuerpo del mensaje
-                thumbnailUrl: thumbnailUrl || "https://i.postimg.cc/k59W5ZDT/descarga-5.jpg",  // Imagen (miniatura)
+                thumbnailUrl: thumbnailUrl || "https://files.catbox.moe/nz83qq.png",  // Imagen (miniatura)
                 mediaType: 2,  // Tipo de medio (2 para enlaces, 1 para imágenes, etc.)
-                mediaUrl: mediaUrl || "https://github.com/SoySapo6/MaycolAI",  // Enlace al canal o cualquier URL
-                sourceUrl: mediaUrl || "https://youtu.be/HFNhMzYmA5Y?si=A_TvnaF41fU9_hXw"  // Fuente del enlace
+                mediaUrl: mediaUrl || "https://whatsapp.com/channel/0029VayXJte65yD6LQGiRB0R",  // Enlace al canal o cualquier URL
+                sourceUrl: mediaUrl || "https://soymaycol.vercel.app/"  // Fuente del enlace
             }
         }
     };
