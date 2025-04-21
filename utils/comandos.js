387dfc30959adfc7621d325dc901675ec038034d
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-04-20 22:40:39 -0500

    SoyMaycol

--- utils/comandos.js
+++ utils/comandos.js
@@ -1,5 +1,5 @@
 exports.comandos = [
-  "menu", "ping", "play", "sticker", "qr", "imagen", "chatgpt",
+  "menu", "ping", "play", "sticker", "qr", "imagen",
   "youtube", "descargar", "twitter", "insta", "facebook",
   "antilink", "ban", "hidetag", "attp", "roleplay", "chiste", "gay",
   "guapo", "besar", "dog", "simi", "ejecutar", "ejecutarjs", "ejecutarpy",
@@ -8,5 +8,6 @@ exports.comandos = [
   "music", "spotify", "info", "apk", "karkat alimentar", "karkat jugar",
   "karkat dormir", "karkat curar", "karkat bañar", "ia", "deepseek",
   "nsfw", "cai", "miaai", "beastai", "stream transmitir", "stream jugar",
-  "stream chat", "stream ganar", "stream perder", "off", "on", "trolear"
+  "stream chat", "stream ganar", "stream perder", "off", "on", "pooh", "sadcat",
+  "oogway", "trolear"
 ];
